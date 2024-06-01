40f716bd9f5818c0419fccbdb0e488b9df8b9912
subeeen <142085729+subeeen@users.noreply.github.com>
2024-06-01 17:27:15 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
 	string a;
 	cin >> a;
-	for (int i = 0; i <= a.length() / 2; i++) {
-		if (a[i] != a[a.length() - i - 1]) {
+	for (int i = 0; i < a.length() / 2; i++) {
+		if (a[i] != a[a.length() - 1 - i]) {
 			cout << 0;
 			return 0;
 		}
