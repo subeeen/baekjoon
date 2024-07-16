7486a83d8e74d4ab479f371a38535f2ad7437457
subeeen <142085729+subeeen@users.noreply.github.com>
2024-07-16 17:07:51 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	string s;
+	cin >> t;
+	while (t--) {
+		cin >> s;
+		cout << s[0] << s[s.length() - 1] <<'\n';
+	}
+	return 0;
+}
