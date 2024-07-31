683b8ba7d889533ac05393537a49791cd1e6a62e
subeeen <142085729+subeeen@users.noreply.github.com>
2024-07-31 17:18:17 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -8,10 +8,7 @@ int main() {
 
 	if ((v - a) % (a - b) == 0) count = (v - a) / (a - b);
 	else count = (v - a) / (a - b) + 1;
-	//v-a : 전체막대-낮에올라가는길이
-	//a-b : 낮에 올라가고 밤에 다시 내려가는 (하루동안의 길이)
-	//(v-a)/(a-b)하면 올라가는 날짜수가 됨
 
-	cout << count+1;
+	cout << count + 1;
 	return 0;
 }
