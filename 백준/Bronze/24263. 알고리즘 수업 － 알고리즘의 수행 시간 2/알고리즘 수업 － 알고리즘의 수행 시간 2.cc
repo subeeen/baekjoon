f6bcb662ce89a17a90bb510f35049e7f71407d98
subeeen <142085729+subeeen@users.noreply.github.com>
2024-08-18 16:19:32 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main() {
+    /*MenOfPassion(A[], n) {
+        sum < -0;
+        for i < -1 to n
+            sum < -sum + A[i]; # 코드1
+            return sum;
+    }*/
+    //시간 복잡도 O(n)
+    int n;
+    cin >> n;
+    cout << n<<"\n"<< 1;
+    return 0;
+}
